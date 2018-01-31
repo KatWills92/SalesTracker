bd6867156bfe840580c418bd20bc98260c3893d9
Katelyn Williams <willi957@ad.nmc.edu>
2018-01-31 16:21:05 -0500

    Add project files.

---
+++ TravelingSalesperson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//*******************************************
+// Katelyn 'kat' Williams
+// CIT195 - Spring 2018
+// The Traveling Salesperson / Sales Tracker
+// 1.29.1018
+//*******************************************
+
+
+namespace TheTravelingSalesperson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller appController = new Controller();
+        }
+    }
+}
